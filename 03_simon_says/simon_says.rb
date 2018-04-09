1ab269fba5a2618abd3f74e7f1d4a7060ed83d50
guillaume <g.delory62@gmail.com>
2018-04-09 18:48:29 +0200

    Exercice 4 en cours

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,11 +1,19 @@
 def echo (string)
-    string
+	string
 end
 
 def shout (string)
-    string.upcase
+	string.upcase
 end
 
 def repeat (string, n = 2)
-    [string] * n * ' '
+	[string] * n * ' '
 end
+
+def start_of_word (string, n)
+	
+	string.split(//) 
+	string[n-1]
+
+end
+
